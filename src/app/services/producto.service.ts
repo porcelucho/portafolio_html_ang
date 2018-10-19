10ca1b3c68087d4264c74cb5076a130d94e7135d
Jorge Higuita <jorge.higuita.87@gmail.com>
2018-10-19 10:59:45 -0500

    se adiciona archivo interface del producto

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class ProductoService {
 
   private cargarProductos() {
     this.http.get('https://angular-html-477d6.firebaseio.com/productos_idx.json')
-        .subscribe( resp => {
+        .subscribe( (resp: ProductoInterface[]) => {
         console.log(resp);
     });
   }
